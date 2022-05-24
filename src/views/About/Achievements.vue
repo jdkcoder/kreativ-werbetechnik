551fda246540c601dca5a9dc6c9e05e829ec9af1
Vũ Quang Minh <jokerdarkknight@protonmail.com>
2022-05-24 20:18:13 +0700

    update

--- src/views/About/Achievements.vue
+++ src/views/About/Achievements.vue
@@ -4,14 +4,40 @@
     <BigTitle :text="text" :dark="true" />
     <h1>ANDERS UND ERFAHREN</h1>
     <p>EINE AGENTUR DIE SIE VERSTEHT UND IHRE WÜNSCHE IN ERFÜLLUNG BRINGT.</p>
+    <div id="achievements-grid">
+      <div v-for="(item, index) in achievements" :key="index">
+        <i :class="'icon ' + item.icon"></i>
+        <span>{{ item.numb }}</span>
+        <strong>{{ item.text }}</strong>
+      </div>
+    </div>
   </section>
 </template>
 
  <style lang='postcss' scoped>
 #achievements {
-  @apply text-center pb-12;
+  @apply text-center pb-20;
   background: #1b1d1f;
   color: #777;
+}
+#achievements-grid {
+  --columns: 1;
+  @apply grid gap-12 mt-8;
+  grid-template-columns: repeat(var(--columns), 1fr);
+  color: #444;
+
+  div {
+    @apply flex flex-col gap-2;
+
+    span {
+      @apply text-4xl font-bold;
+      color: #777;
+    }
+  }
+
+  .icon {
+    @apply text-4xl;
+  }
 }
 
 h1 {
@@ -35,7 +61,7 @@ h1 {
 }
 
 p {
-    @apply mx-4
+  @apply mx-4;
 }
 </style> 
 
@@ -48,6 +74,23 @@ export default {
     return {
       index: 0,
       text: "Werbung, die Sie nach vorne bringt!",
+      achievements: [
+        {
+          icon: "fa-regular fa-thumbs-up",
+          numb: 800,
+          text: "Erfolgreiche Projekte",
+        },
+        {
+          icon: "fa-solid fa-heart",
+          numb: 400,
+          text: "Glückliche Kunden",
+        },
+        {
+          icon: "fa-solid fa-flag",
+          numb: 16,
+          text: "Jahre Erfahrung.",
+        },
+      ],
     };
   },
 
