15f0951ec2c87733111bce9c919706be6b43c97f
Vũ Quang Minh <jokerdarkknight@protonmail.com>
2022-05-25 18:23:58 +0700

    update

--- src/components/Navs/mNav.vue
+++ src/components/Navs/mNav.vue
@@ -19,8 +19,22 @@
       <li class="item">
         <RouterLink to="/ueberuns/">Über Uns</RouterLink>
       </li>
-      <li class="item">
+      <li class="item dropdown" @click="dropdownTriggered($event)">
         <RouterLink to="/services/">Leistungen</RouterLink>
+        <button type="button" class="arrow">
+          <i class="fa-solid fa-caret-down"></i>
+        </button>
+
+        <ul class="sub-menu">
+          <li>
+            <RouterLink to="/leuchtkasten/"> Leuchtkasten </RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/fahrzeugbeschriftung/">
+              Fahrzeugbeschriftung
+            </RouterLink>
+          </li>
+        </ul>
       </li>
       <li class="item">
         <RouterLink to="/contact/">Kontakt</RouterLink>
@@ -75,7 +89,38 @@
   .item {
     @apply p-4 cursor-pointer;
     border-bottom: 1px solid rgba(51, 51, 51, 1);
+
+    &.dropdown {
+      overflow: hidden;
+      height: 3rem;
+      transition: height 0.25s ease-in-out;
+
+      &.active {
+        .sub-menu {
+          overflow-y: auto;
+          height: 5.25rem;
+          @apply mt-4;
+
+          li {
+            @apply py-4;
+            border-bottom: 1px solid rgba(51, 51, 51, 1);
+
+            &:first-of-type {
+              border-top: 1px solid rgba(51, 51, 51, 1);
+            }
+          }
+
+          &::-webkit-scrollbar {
+            width: 0;
+            height: 0;
+          }
+        }
+      }
+    }
   }
+}
+ul.sub-menu {
+  @apply mt-4;
 }
 #toggle-mnav {
   @apply relative text-xl mr-4;
@@ -94,6 +139,9 @@
     @apply opacity-0;
   }
 }
+.arrow {
+  @apply ml-2;
+}
 </style> 
 
 <script>
@@ -101,16 +149,27 @@ export default {
   components: {},
 
   data() {
-    return {};
+    return { z: false };
   },
 
   methods: {
     toggleNav() {
       document.querySelector("#mobile-nav").classList.toggle("active");
     },
+    dropdownTriggered(e) {
+      this.z = !this.z;
+      let slideDown = (el) => (el.style.height = `${el.scrollHeight}px`);
+      if (this.z) {
+        slideDown(e.currentTarget.closest(".dropdown"));
+        e.currentTarget.closest(".dropdown").classList.add("active");
+      } else {
+        e.currentTarget.closest(".dropdown").removeAttribute("style");
+        e.currentTarget.closest("dropdown").classList.remove("active");
+      }
+    },
   },
   mounted() {
-    let items = document.querySelectorAll("#mnav-items > .item");
+    let items = document.querySelectorAll("#mnav-items > .item > a");
     for (let item of items) {
       item.addEventListener("click", () => {
         document.querySelector("#mobile-nav").classList.remove("active");
